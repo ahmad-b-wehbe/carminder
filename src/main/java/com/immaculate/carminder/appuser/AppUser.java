919ff6f95fb7debdbceadccaa4c697999f29c195
ahmad wehbe <ahmdbwhbe@gmail.com>
2024-06-22 13:36:56 +0300

    add AppUser Entity and implemented UserDetails to manage users

---
+++ src/main/java/com/immaculate/carminder/appuser/AppUser.java
@@ -0,0 +1,70 @@
+package com.immaculate.carminder.appuser;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class AppUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+    private Boolean locked = false;
+    private Boolean enabled = false;
+
+    public AppUser(String name, String email, String password, AppUserRole appUserRole) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(appUserRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ src/main/java/com/immaculate/carminder/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.immaculate.carminder.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
